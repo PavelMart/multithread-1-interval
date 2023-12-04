35eb8233eb8e62d8d48a50924992765f3eb1fbd6
PavelMart <martackovp@yandex.ru>
2023-12-04 22:37:18 +0300

    Задание со звездочкой выполнено

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,69 @@
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
+    static final Integer length = 25;
 
-    public static void main(String[] args) throws InterruptedException {
-        String[] texts = new String[25];
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        String[] texts = new String[length];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
 
-        List<Thread> threads = new ArrayList<>();
+        List<Future<Long>> threads = new ArrayList<>();
+
+        final ExecutorService threadPool = Executors.newFixedThreadPool(length);
 
         for (String text : texts) {
-            Thread thread = new Thread(() -> {
-                long startTs = System.currentTimeMillis();
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
+            final Future<Long> task = threadPool.submit(() -> {
+                        long startTs = System.currentTimeMillis();
+                        int maxSize = 0;
+                        for (int i = 0; i < text.length(); i++) {
+                            for (int j = 0; j < text.length(); j++) {
+                                if (i >= j) {
+                                    continue;
+                                }
+                                boolean bFound = false;
+                                for (int k = i; k < j; k++) {
+                                    if (text.charAt(k) == 'b') {
+                                        bFound = true;
+                                        break;
+                                    }
+                                }
+                                if (!bFound && maxSize < j - i) {
+                                    maxSize = j - i;
+                                }
                             }
                         }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
+//                        System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                        long endTs = System.currentTimeMillis();
+//                        System.out.println("Time: " + (endTs - startTs) + "ms");
+                        return endTs - startTs;
                     }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-                long endTs = System.currentTimeMillis();
-                System.out.println("Time: " + (endTs - startTs) + "ms");
-            });
-            threads.add(thread);
-            thread.start();
+            );
+            threads.add(task);
         }
 
-        for (Thread thread : threads) {
-            thread.join();
+        List<Long> results = new ArrayList<>();
+
+        for (Future task : threads) {
+            Long time = (Long) task.get();
+            System.out.println("Time: " + (time) + "ms");
+            results.add(time);
         }
+
+        Long res = results
+                .stream()
+                .max((a, b) -> {
+                    if (a > b) return 1;
+                    if (a < b) return -1;
+                    return 0;
+                })
+                .get();
+
+        System.out.println("Max time is " + res + "ms");
+
+        threadPool.shutdown();
     }
 
     public static String generateText(String letters, int length) {
